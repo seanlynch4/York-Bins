62bcee9f383fb4c6e97b5e7a3ee91bad2dddc92c
Sean <Sean@seans-macbook-pro.local>
2016-07-12 14:11:45 +0100

    Initial Commit

---
+++ York Bin Collection/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  York Bin Collection
+//
+//  Created by Sean on 12/07/2016.
+//  Copyright © 2016 Sean. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ York Bin CollectionTests/York_Bin_CollectionTests.swift
@@ -0,0 +1,36 @@
+//
+//  York_Bin_CollectionTests.swift
+//  York Bin CollectionTests
+//
+//  Created by Sean on 12/07/2016.
+//  Copyright © 2016 Sean. All rights reserved.
+//
+
+import XCTest
+@testable import York_Bin_Collection
+
+class York_Bin_CollectionTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
